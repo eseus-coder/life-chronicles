9bde955dbf9ed69f9d7c8984229b2bd5e7d45519
eseus-coder <arindambose975@gmail.com>
2024-03-24 23:27:31 +0530

    - Basic setup for styles (to be updated) - Basic setup for redux (to be updated) - Basic setup for interceptors (to be updated) - Basic setup for sagas (to be updated) - Basic setup for components (to be updated)

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import lifeChronicles from './assets/images/lifeChronicles.png';
+import Posts from './components/Posts/Posts';
+import Form from './components/Forms/Form';
+import useStyles from './styles';
 
 const App = () => {
+
+    const classes = useStyles();
+
     return (
-        <div>
-            <h1>Frontend side</h1>
-        </div>
+        <Container maxWidth='lg'>
+            <AppBar className={classes.appBar} position='static' color='inherit'>
+                <Typography className={classes.heading} variant='h2' align='center'>Life Chronicles</Typography>
+                <img className={classes.image} src={lifeChronicles} alt='Life Chronicles' height="60"/>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justifyContent='space-between' alignItems='center' spacing={4}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item>
+                            <Form />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     )
 };
 
---
+++ client/src/components/Forms/Form.js
@@ -0,0 +1,15 @@
+import React from "react";
+import useStyles from './styles';
+
+const Form = () => {
+
+    const classes = useStyles();
+    
+    return (
+        <div>
+            <h1>Form Component</h1>
+        </div>
+    );
+};
+
+export default Form;
